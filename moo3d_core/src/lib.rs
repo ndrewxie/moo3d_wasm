146f994e664dcd83c110586271412a7e61b91a7e
yks <ndrew.xie@gmail.com>
2021-09-05 18:30:00 +0000

    Updated default scene for profiling

--- moo3d_core/src/lib.rs
+++ moo3d_core/src/lib.rs
@@ -56,8 +56,8 @@ impl GameState {
         let scale = self.renderer.scale as isize;
         self.renderer.clear();
 
-        for i in -15..15 {
-            for j in -20..15 {
+        for i in -30..30 {
+            for j in -35..30 {
                 self.renderer.draw_cubeface(
                     &Point3D::from_euc_coords(
                         center_x as isize + scale * i,
@@ -65,6 +65,29 @@ impl GameState {
                         5 * near + scale * j,
                     ),
                     5,
+                    &[scale as f32 * 0.5, scale as f32 * 0.5, scale as f32 * 0.5],
+                    &RenderMatrices::identity(),
+                    if i % 2 == 0 { 1 } else { 0 },
+                );
+            }
+        }
+    }
+    pub fn render_facewall(&mut self, _curr_time: usize) {
+        let center_x = self.renderer.width / 2;
+        let center_y = self.renderer.height / 2;
+        let near = self.renderer.get_near() as isize;
+        let scale = self.renderer.scale as isize;
+        self.renderer.clear();
+
+        for i in -30..30 {
+            for j in -30..30 {
+                self.renderer.draw_cubeface(
+                    &Point3D::from_euc_coords(
+                        center_x as isize + scale * i,
+                        center_y as isize + j * scale,
+                        5 * near,
+                    ),
+                    6,
                     &[scale as f32 * 0.5, scale as f32 * 0.5, scale as f32 * 0.5],
                     &RenderMatrices::identity(),
                     if i % 2 == 0 { 1 } else { 0 },
@@ -90,7 +113,8 @@ impl GameState {
     pub fn render(&mut self, curr_time: usize) {
         //self.render_spinningcube(curr_time);
         //self.render_cubeplane(curr_time);
-        self.render_faceplane(curr_time);
+        //self.render_faceplane(curr_time);
+        self.render_facewall(curr_time);
     }
     pub fn translate_camera(&mut self, trans_x: isize, trans_y: isize, trans_z: isize) {
         self.renderer.camera.translate(trans_x, trans_y, trans_z);
